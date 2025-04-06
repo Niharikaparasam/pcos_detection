c29927fe2dab498221e1a1a85643d96ef1f8f83a
Niharikaparasam <niharikaparasam18@gmail.com>
2025-04-06 22:56:19 +0530

    Modified index

--- backend/frontend.py
+++ backend/frontend.py
@@ -1,10 +1,7 @@
 from flask import Flask, send_from_directory
-from flask_cors import CORS  # <--- import this
 import os
 
 app = Flask(__name__, static_folder='static')
-CORS(app)  
-
 
 @app.route('/')
 def index():
